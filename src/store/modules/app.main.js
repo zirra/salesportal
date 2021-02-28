3ec7fb6e1ded2303afb6eb48d030dfaa1776980b
zirra <msullivan@heysulli.com>
2021-02-28 09:46:33 -0600

    adding hook for border radius

--- src/store/modules/app.main.js
+++ src/store/modules/app.main.js
@@ -1,10 +1,11 @@
 const state = {
+  borderRadius: false,
+  borderSize: 'sm',
   color1: null,
   color2: null,
   color3: null,
   color4: null,
-  color5: null,
-  borderSize: 'sm'
+  color5: null
 }
 
 const mutations = {
@@ -20,12 +21,13 @@ const actions = {
 }
 
 const getters = {
+  borderRadius: (state) => { return state.borderRadius },
   borderSize: (state) => { return state.borderSize },
   color1: (state) => { return `#${state.color1}` },
   color2: (state) => { return `#${state.color2}` },
   color3: (state) => { return `#${state.color3}` },
   color4: (state) => { return `#${state.color4}` },
-  color5: (state) => { return `#${state.color5}` },
+  color5: (state) => { return `#${state.color5}` }
 }
 
 export default {
