c7f06f27a6cd0ad8acd32e4680a2d51ec4e2f194
zirra <msullivan@heysulli.com>
2021-02-28 09:41:33 -0600

    dynamic style setting

--- src/store/modules/app.main.js
+++ src/store/modules/app.main.js
@@ -3,12 +3,16 @@ const state = {
   color2: null,
   color3: null,
   color4: null,
-  color5: null
+  color5: null,
+  borderSize: 'sm'
 }
 
 const mutations = {
   mutateColor(state, obj) {
     state[obj.did] = obj.value
+  },
+  mutateBorderSize(state, size) {
+    state.borderSize = size
   }
 }
 
@@ -16,11 +20,12 @@ const actions = {
 }
 
 const getters = {
+  borderSize: (state) => { return state.borderSize },
   color1: (state) => { return `#${state.color1}` },
   color2: (state) => { return `#${state.color2}` },
   color3: (state) => { return `#${state.color3}` },
   color4: (state) => { return `#${state.color4}` },
-  color5: (state) => { return `#${state.color5}` }
+  color5: (state) => { return `#${state.color5}` },
 }
 
 export default {
